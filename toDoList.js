828782055a7da1b1e556988271b68d654e2ca8cf
Patrick Rallings <patrickrallings@Patricks-iMac.local>
2022-04-01 11:33:30 -0400

    Added show button

--- toDoList.js
+++ toDoList.js
@@ -1,7 +1,18 @@
 const listofToDoItems = document.getElementById('listActual')
 const listInput = document.querySelector('input')
 const inputButton = document.getElementById('addButton')
+const showButton = document.getElementById('showButton')
 let newItem = document.createElement('li')
+
+showButton.addEventListener('click', function() {
+    if (showButton.innerText === 'Show Daily Items') {
+        showButton.innerText = 'Hide Daily Items'
+        document.querySelectorAll('.permItem').forEach(a=>a.style.display = 'block');
+    } else {
+            showButton.innerText = 'Show Daily Items'
+            document.querySelectorAll('.permItem').forEach(a=>a.style.display = 'none');
+    }
+})
 
 inputButton.addEventListener('click', function() {
     let newItem = document.createElement('li')
